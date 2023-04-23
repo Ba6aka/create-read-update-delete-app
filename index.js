de5e879bb276742b93faedced8289b6dd2f7f314
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-23 18:36:25 +0300

    add router for /getAllFiles

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const { createServer } = require('http');
 const { readFileSync, writeFileSync, readdir, readdirSync } = require('fs');
+const { getDb, connectToDb, } = require('./db')
+const { ObjectId } = require('mongodb')
 
-//! Create and listen server
 createServer(async function (req, res) {
+
     if (req.method == 'GET') {
         serveFile(req.url, res)
     }
 }).listen(process.env.PORT || 1337)
 
 
-function serveFile(url, res) {
+
+
+
+
+
+async function serveFile(url, res) {
     let path = url
     let file
+
+    connectToDb((err) => {
+        if (!err) {
+            db = getDb()
+        }
+    })
+
     if (path == '/') {
         file = readFileSync(__dirname + '/public/' + '/index.html')
-    } else {
-        file = readFileSync(__dirname + '/public' + path)
+    } else if (path == '/getAllFilles') {
+        let words = []
+
+        await db.collection('CRUD')
+            .find()
+            .forEach(element => {
+                words.push(element)
+            })
+
+        file = (JSON.stringify(words))
+    }
+    else {
+        file = readFileSync(__dirname + '/public/' + path)
     }
     res.end(file)
 }
