f3b7f5527daf627d14d20ee421521fc25ac9f6c4
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-24 17:58:51 +0300

    add function updateWord

--- public/script.js
+++ public/script.js
@@ -1,47 +1,56 @@
 const voc = document.querySelector('.vocabluary')
 const form = document.querySelector('form')
+const btn = form.querySelector('button')
+const uaInput = form.querySelector('.ua')
+const engInput = form.querySelector('.eng')
+const shortInput = form.querySelector('.short')
+const longInput = form.querySelector('.long')
+
+getData('/getAllFilles')
+    .then((result) => {
+        render(result)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 
-    getData('/getAllFilles')
-        .then((result) => {
-            render(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+voc.addEventListener('click', (e) => {
+    const id = e.target.parentElement.parentElement.id
+    if (e.target.className == 'trash') {
+        deleteData(`/deleteWord/${id}`)
+            .then((result) => {
+                console.log(result)
+                e.target.parentElement.parentElement.remove()
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    } else if (e.target.className = 'update') {
+        getData(`/findOneWord/${id}`)
+            .then((result) => {
+
+                btn.innerText = 'update'
+                btn.className = 'update'
+                btn.id = id
+
+                uaInput.value = result['ua']
+                engInput.value = result['eng']
+                shortInput.value = result['short']
+                longInput.value = result['long']
+            })
+    }
+})
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const formData = new FormData(form)
-
-    const data = {
-        ua: formData.get('ua'),
-        eng: formData.get('eng'),
-        short: formData.get('short'),
-        long: formData.get('descr')
-    }
-    console.log(data)
-    postData('/postWord', data)
-        .then((result) => {
-            console.log(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-voc.addEventListener('click', (e) =>{
-    if (e.target.className == 'trash'){
-        const id = e.target.parentElement.parentElement.id
-        console.log(id)
-        deletedDta(`/deleteWord/${id}`)
-        .then((result) =>{
-            console.log(result)
-            e.target.parentElement.parentElement.remove()
-        })
-        .catch((err) =>{
-            console.log(err)
-        })
+    if (btn.className == 'update') {
+        console.log('update')
+        updateWord()
+    } else {
+        postNewWord()
     }
 })
 
@@ -62,4 +71,47 @@ function render(data) {
         `
         voc.append(div)
     }
+}
+
+
+function postNewWord() {
+    const formData = new FormData(form)
+
+    postData('/postWord', getDataObject(formData))
+        .then((result) => {
+            console.log(result)
+            form.reset()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+
+function getDataObject(formData) {
+    const data = {
+        ua: formData.get('ua'),
+        eng: formData.get('eng'),
+        short: formData.get('short'),
+        long: formData.get('descr')
+    }
+
+    return data
+}
+
+function updateWord() {
+    const formData = new FormData(form)
+    console.log(getDataObject(formData))
+    updateData(`/updateword/${btn.id}`, getDataObject(formData))
+        .then((result) => {
+            console.log(result)
+            form.reset()
+            btn.innerText = 'send form'
+            btn.className = ''
+            btn.id = ''
+           
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
