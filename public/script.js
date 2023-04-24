5bda39bfbd4c4d9afea1fc888893a84ab5a32565
Ba6aka <bam6ykba6aka@gmail.com>
2023-04-24 14:34:02 +0300

    add clases in render

--- public/script.js
+++ public/script.js
@@ -2,13 +2,13 @@ const voc = document.querySelector('.vocabluary')
 const form = document.querySelector('form')
 
 
-getData('/getAllFilles')
-    .then((result) => {
-        render(result)
-    })
-    .catch((err) =>{
-        console.log(err)
-    })
+    getData('/getAllFilles')
+        .then((result) => {
+            render(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -22,15 +22,28 @@ form.addEventListener('submit', (e) => {
     }
     console.log(data)
     postData('/postWord', data)
-    .then((result) =>{
-        console.log(result)
-    })
-    .catch((err) =>{
-        console.log(err)
-    })
+        .then((result) => {
+            console.log(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
-
+voc.addEventListener('click', (e) =>{
+    if (e.target.className == 'trash'){
+        const id = e.target.parentElement.parentElement.id
+        console.log(id)
+        deletedDta(`/deleteWord/${id}`)
+        .then((result) =>{
+            console.log(result)
+            e.target.parentElement.parentElement.remove()
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+    }
+})
 
 function render(data) {
     for (const word of data) {
@@ -42,13 +55,11 @@ function render(data) {
                 <span>${word['eng']}</span>
                 <span>${word['short']}</span>
                 <span class="actions">
-                    <img src="images/icon-trash.png"></img>
-                    <img src="images/icon-update.png"></img>
-                    <img src="images/icon-view.png"></img>
+                    <img class="trash" src="images/icon-trash.png"></img>
+                    <img class="update" src="images/icon-update.png"></img>
+                    <img class="view" src="images/icon-view.png"></img>
                 </span>
-            
         `
-
         voc.append(div)
     }
 }
